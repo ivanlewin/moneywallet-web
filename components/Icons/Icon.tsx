a6470aa765ff734221c2254af40333cac0e60fd9
Iván Lewin <57846638+ivanlewin@users.noreply.github.com>
2022-12-15 01:33:43 -0300

    map all icons to mui icons

---
+++ components/Icons/Icon.tsx
@@ -0,0 +1,41 @@
+import { Avatar, Icon as MUIIcon } from "@mui/material";
+import type { Icon as IconType } from "types";
+import { iconMapping } from "./icon-mapping";
+
+export default function Icon(props: IconType) {
+  if (props.type === 'color') {
+    return (
+      <Avatar sx={{ bgcolor: props.color }}>{props.name}</Avatar>
+    );
+  } else {
+    const icon = iconMapping[props.resource];
+    if (icon) {
+      return (
+        <MUIIcon
+          sx={{
+            bgcolor: '#2196F3',
+            color: '#FFF',
+            height: 40,
+            width: 40,
+            p: '0.3em',
+            fontSize: '1.25rem',
+            listStyle: 'none',
+            boxSizing: 'border-box',
+            position: 'relative',
+            flexShrink: 0,
+            lineHeight: 1,
+            borderRadius: '50%',
+            overflow: 'hidden',
+            userSelect: 'none',
+          }}
+        >
+          {icon}
+        </MUIIcon>
+      );
+    } else {
+      return (
+        <Avatar>{props.resource}</Avatar>
+      );
+    }
+  }
+}
